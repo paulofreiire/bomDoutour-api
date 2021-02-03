8ff5bbb08b73a9ea1ea8fa9494b12ebaf59da023
Paulo Freire <paulo.freiire@hotmail.com>
2021-02-03 00:01:50 -0300

    pacients done

---
+++ app/Models/Pacient.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pacient extends Model
+{
+    use HasFactory;
+    protected $table = 'pacients';
+
+    protected $fillable = [
+        'name',
+        'gender',
+        'dateBirt',
+        'document'
+    ];
+}
